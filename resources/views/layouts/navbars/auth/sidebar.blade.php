1d7e96c21da5ae98749c94e87b2b258da9b2d77c
mali2927 <mali2927@gmail.com>
2025-05-07 12:46:53 +0500

    remove unnessary things some

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -279,7 +279,7 @@
         <div class="card card-background shadow-none card-background-mask-secondary" id="sidenavCard">
             <div class="full-background" style="background-image: url('../assets/img/curved-images/white-curved.jpeg')">
             </div>
-            <div class="card-body text-left p-3 w-100">
+            <!-- <div class="card-body text-left p-3 w-100">
                 <div
                     class="icon icon-shape icon-sm bg-white shadow text-center mb-3 d-flex align-items-center justify-content-center border-radius-md">
                     <i class="ni ni-diamond text-dark text-gradient text-lg top-0" aria-hidden="true"
@@ -291,7 +291,7 @@
                     <a href="/documentation/bootstrap/overview/soft-ui-dashboard/index.html" target="_blank"
                         class="btn btn-white btn-sm w-100 mb-0">Documentation</a>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </aside>
