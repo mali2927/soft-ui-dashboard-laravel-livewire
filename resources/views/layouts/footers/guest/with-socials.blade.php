2a16d84323ba24c2bdf9c6b17593609a7d4f0b94
mali2927 <mali2927@gmail.com>
2025-05-29 13:53:22 +0500

    unnesacary things removed

--- resources/views/layouts/footers/guest/with-socials.blade.php
+++ resources/views/layouts/footers/guest/with-socials.blade.php
@@ -1,9 +1,8 @@
   <footer class="footer pb-5">
       <div class="container">
-          <div class="row">
+          {{-- <div class="row">
               <div class="col-lg-8 mb-4 mx-auto text-center">
-                  <a href="https://www.creative-tim.com" target="_blank"
-                      class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
+                  <a href="https://www.creative-tim.com" target="_blank" class="text-secondary me-xl-5 me-3 mb-sm-0 mb-2">
                       Company
                   </a>
                   <a href="https://www.creative-tim.com/presentation" target="_blank"
@@ -46,13 +45,12 @@
                       <span class="text-lg fab fa-github"></span>
                   </a>
               </div>
-          </div>
+          </div> --}}
           <div class="row">
               <div class="col-8 mx-auto text-center mt-1">
                   <p class="mb-0 text-secondary">
-                      Copyright © {{ now()->year }} Soft by <a style="color: #252f40;" href="https://www.creative-tim.com"
-                          class="font-weight-bold ml-1" target="_blank">Creative Tim</a> &amp; <a style="color: #252f40;"
-                          href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">UPDIVISION</a>
+                      Copyright © {{ now()->year }} Powered by <a style="color: #252f40;" href="https://novtek.co.uk/"
+                          class="font-weight-bold ml-1" target="_blank">Novtek</a>
               </div>
               </p>
           </div>
