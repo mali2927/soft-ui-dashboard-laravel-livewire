973ab01bb1239094a6d49713c0eac44116a7821e
mali2927 <mali2927@gmail.com>
2025-06-04 11:32:37 +0500

    navbar changes

--- resources/views/layouts/navbars/auth/nav.blade.php
+++ resources/views/layouts/navbars/auth/nav.blade.php
@@ -9,8 +9,8 @@
                     <li class="breadcrumb-item text-sm text-dark active text-capitalize" aria-current="page">
                         {{ str_replace('-', ' ', Route::currentRouteName()) }}</li>
                 </ol>
-                <h6 class="font-weight-bolder mb-0 text-capitalize">
-                    {{ str_replace('-', ' ', Route::currentRouteName()) }}</h6>
+                {{-- <h6 class="font-weight-bolder mb-0 text-capitalize">
+                    {{ str_replace('-', ' ', Route::currentRouteName()) }}</h6> --}}
             </nav>
             <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4 d-flex justify-content-end"
                 id="navbar">
